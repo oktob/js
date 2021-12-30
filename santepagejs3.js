9b8fcd891361c41b5d87053edf0864b40ca3720f
oktob <58049913+oktob@users.noreply.github.com>
2021-12-30 18:50:35 +0100

    Update santepagejs3.js

--- santepagejs3.js
+++ santepagejs3.js
@@ -1,4 +1,2 @@
-  //<![CDATA[  
 function loadCSS(e, t, n) { "use strict"; var i = window.document.createElement("link"); var o = t || window.document.getElementsByTagName("script")[0]; i.rel = "stylesheet"; i.href = e; i.media = "only x"; o.parentNode.insertBefore(i, o); setTimeout(function () { i.media = n || "all" }) }  
-loadCSS("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css");  
-//]]>
+loadCSS("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css");
