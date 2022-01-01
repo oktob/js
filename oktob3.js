6c50e00d6e398fde291aa2e5c78bef84614377d0
oktob <58049913+oktob@users.noreply.github.com>
2022-01-01 17:03:37 +0100

    Update oktob3.js

--- oktob3.js
+++ oktob3.js
@@ -3,5 +3,18 @@
 
 function downloadJSAtOnload(){var e=document.createElement("script");e.src="https://cdn.jsdelivr.net/gh/oktob/js@master/adsensegurd.js",document.body.appendChild(e)}window.addEventListener?window.addEventListener("load",downloadJSAtOnload,!1):window.attachEvent?window.attachEvent("onload",downloadJSAtOnload):window.onload=downloadJSAtOnload;
 
-
+(function () {
+   document.addEventListener("DOMContentLoaded", function () {
+       var e = "dmca-badge";
+       var t = "refurl";
+       var n = document.querySelectorAll('a.'+e);
+       if (n[0].getAttribute("href").indexOf("refurl") < 0) {
+           for (var r = 0; r < n.length; r++) {
+               var i = n[r];
+               i.href = i.href + (i.href.indexOf("?") === -1 ? "?" : "&") + t + "=" + document.location
+           }
+       }
+   }, false)
+}
+)()
 /*]]>*/
